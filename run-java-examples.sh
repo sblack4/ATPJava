36bba37f5283b152ca835ffc86201eb24ace34d3
sblack4 <genseb7@gmail.com>
2018-10-04 17:16:06 -0400

    fixed example runner script

--- run-java-examples.sh
+++ run-java-examples.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-JDBC_LIB="/opt/oracle/tools/java/ojdbc8-full"
+JDBC_LIB="/opt/oracle/tools/java/ojdbc8-full/*"
 JAVA_SDK="/opt/oracle/tools/java/sdk"
-CLASSPATH="$JDBC_LIB:$DIR:$JAVA_SDK/lib:$JAVA_SDK/third-party/lib:$DIR/atp-rest-scripts.jar"
+SDK_LIB="$JAVA_SDK/lib/*:$JAVA_SDK/third-party-lib/*"
+CLASSPATH="$JDBC_LIB:$SDK_LIB:$DIR/atp-rest-scripts.jar"
 
 java -cp "$CLASSPATH" com.github.sblack4.CLI "$@"
