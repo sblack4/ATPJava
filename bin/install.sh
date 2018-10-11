700fb368e34d4fea74588f12e80937e40f7bb559
sblack4 <genseb7@gmail.com>
2018-10-11 17:38:28 -0400

    changed installer

--- bin/install.sh
+++ bin/install.sh
@@ -12,7 +12,7 @@ homedir=~
 eval homedir=${homedir}
 APP_DIR=${ARG1:-"$homedir"}
 
-# get jdk
+echo "Installing JDK"
 yum update && yum install -y java-1.8.0-openjdk.x86_64
 yum install -y java-1.8.0-openjdk.x86_64
 
@@ -23,11 +23,17 @@ cd ATPJava
 
 # include --content-disposition
 # because otherwise it names file with params (eg ATPJava.tar.gz?raw=true)
-wget --content-disposition https://github.com/sblack4/ATP-REST-Java/blob/master/ATPJava.tar.gz?raw=true
-wget --content-disposition https://github.com/sblack4/ATP-REST-Java/blob/master/lib.tar.gz?raw=true
+echo "downloading App to $APP_DIR"
+wget --content-disposition https://github.com/sblack4/ATP-REST-Java/blob/master/ATPJava.zip?raw=true
+unzip ATPJava.zip
 
-tar xvzf ATPJava.tar.gz
-tar xvzf lib.tar.gz
+echo "downloading libraries to $APP_DIR/lib"
+mkdir lib
+cd lib
+wget --content-disposition https://github.com/sblack4/ATP-REST-Java/blob/master/lib.zip?raw=true -P
+unzip lib.zip
 
 # run it
-./run.sh --help
+echo "try a command"
+cd ${APP_DIR}
+bin/runCLI.sh
